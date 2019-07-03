cd563cf4fad8b43b0b1030d35e30ed639b42a59c
Anurag Prakash <39311333+anuragprakash007@users.noreply.github.com>
2019-07-03 22:54:25 +0530

    Add Delete

--- app.js
+++ app.js
@@ -79,7 +79,17 @@ app.put("/blogs/:id",function(req,res){
 		}
 	});
 });
-
+app.delete("/blogs/:id",function(req,res){
+	Blog.findByIdAndRemove(req.params.id,function(err){
+		if(err)
+		{
+			res.redirect("/blogs");
+		}else{
+			res.redirect("/blogs");
+		}
+	});
+	
+});
 app.listen(3000,process.env.IP,function(){
 	console.log("SERVER IS RUNNING !");
 });
